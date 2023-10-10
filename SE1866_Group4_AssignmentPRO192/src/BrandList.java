094a1478d9f666c8440034294b5ad9921055dd34
hieuntthe180407 <147322462+hieuntthe180407@users.noreply.github.com>
2023-10-10 20:16:41 +0700

    Add files via upload

---
+++ SE1866_Group4_AssignmentPRO192/src/BrandList.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author trong
+ */
+
+import java.util.*;
+import java.lang.*;
+public class BrandList {
+    
+    public BrandList(){
+        
+    }
+public boolean loadFromFile(String){
+    
+}
+public boolean saveToFile(String){
+    
+}
+public int searchID (String ID){
+    
+}
+public Brand getUserChoice(){
+    
+}
+public void addBrand(){
+    
+}
+public void updateBrand(){
+    
+}
+public void listBrands(){
+    
+}
+}
