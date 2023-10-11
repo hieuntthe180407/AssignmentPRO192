3af8d395037d39f911385d78f43134db99b4f52d
hieuntthe180407 <147322462+hieuntthe180407@users.noreply.github.com>
2023-10-11 17:15:59 +0700

    Add files via upload

--- SE1866_Group4_AssignmentPRO192/src/Car.java
+++ SE1866_Group4_AssignmentPRO192/src/Car.java
@@ -3,5 +3,67 @@
 import java.util.*;
 import java.lang.*;
 public class Car {
+String carID;
+Brand brand;
+String color;
+String frameID;
+String engineID;
+
+    public Car() {
+        
+    }
+
+    public Car(String carID, Brand brand, String color, String frameID, String engineID) {
+        this.carID = carID;
+        this.brand = brand;
+        this.color = color;
+        this.frameID = frameID;
+        this.engineID = engineID;
+    }
+
+    public String getCarID() {
+        return carID;
+    }
+
+    public void setCarID(String carID) {
+        this.carID = carID;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFrameID() {
+        return frameID;
+    }
+
+    public void setFrameID(String frameID) {
+        this.frameID = frameID;
+    }
+
+    public String getEngineID() {
+        return engineID;
+    }
+
+    public void setEngineID(String engineID) {
+        this.engineID = engineID;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "carID=" + carID + ", brand=" + brand + ", color=" + color + ", frameID=" + frameID + ", engineID=" + engineID + '}';
+    }
 
 }
--- SE1866_Group4_AssignmentPRO192/src/CarList.java
+++ SE1866_Group4_AssignmentPRO192/src/CarList.java
@@ -3,5 +3,49 @@
 import java.util.*;
 import java.lang.*;
 public class CarList {
-        //testeteteuie;
+       BrandList brandList;
+public CarList(BrandList bList){
+    
+}
+
+public boolean loadFromFile(String){
+    
+}
+
+public boolean saveToFile(String){
+    
+}
+
+public int searchID (String ID){
+    
+}
+
+public int searchFrame (String fID){
+    
+}
+
+public int searchEngine (String eID){
+    
+}
+
+public void addCar(){
+    
+}
+
+public void printBasedBrandName (){
+    
+}
+
+public boolean removeCar(){
+    
+}
+
+public boolean updateCar(){
+    
+}
+
+public void listCars(){
+    
+}
+
 }
