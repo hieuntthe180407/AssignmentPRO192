6f7080e04514bb7d590340a53d4e98a8fc3bc358
TienYung <147519779+TienYung@users.noreply.github.com>
2023-10-18 17:12:13 +0700

    Update CarList.java

--- SE1866_Group4_AssignmentPRO192/src/CarList.java
+++ SE1866_Group4_AssignmentPRO192/src/CarList.java
@@ -22,15 +22,33 @@ public boolean saveToFile(String){
 }
 
 public int searchID (String ID){
-    
+       int N = this.size(); 
+       for (int i = 0; i < N; i++) {
+              if (this.get(i).carID.equals(carID)) {
+        return i;
+        }
+    }
+    return -1;
 }
 
 public int searchFrame (String fID){
-    
+    int N = this.size();
+    for (int i = 0; i < N; i++) {
+       if (this.get(i).getFrameID().equals(fID)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 public int searchEngine (String eID){
-    
+    int N = this.size();
+    for (int i = 0; i < N; i++) {
+    if (this.get(i).getEngineID().equals(eID)) {
+        return i;
+        }
+    }
+    return -1;
 }
 
 public void addCar(){
@@ -42,7 +60,14 @@ public void printBasedBrandName (){
 }
 
 public boolean removeCar(){
-    
+    int pos = searchID(removedID);
+    if (pos < 0) {
+        System.out.println("Not found!");
+        return false;
+    } else {
+        this.remove(pos);
+        return true;
+        }
 }
 
 public boolean updateCar(){
