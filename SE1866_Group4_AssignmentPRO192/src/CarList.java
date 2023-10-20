931846392dc1856751f23e6750ac9112d64a4e4a
Ducanhhh7777 <147521939+Ducanhhh7777@users.noreply.github.com>
2023-10-21 00:05:11 +0700

    Update CarList.java

--- SE1866_Group4_AssignmentPRO192/src/CarList.java
+++ SE1866_Group4_AssignmentPRO192/src/CarList.java
@@ -4,6 +4,19 @@
 import java.util.*;
 import java.lang.*;
 public class CarList {
+       public static void main(String[] args) {
+    BrandList brandList = new BrandList();
+    CarList carList = new CarList(brandList);
+    
+    // Load cars from the file
+    if (carList.loadFromFile("cars.txt")) {
+        // List all cars
+        System.out.println("List all cars");
+        carList.listCars();
+    } else {
+        System.out.println("Failed to load cars from the file.");
+    }
+}
        BrandList brandList;
        List<Car>  cars;
        Car c;
@@ -14,11 +27,50 @@ public CarList(BrandList bList){
 }
 
 public boolean loadFromFile(String){
-    
+     File file = new File(filename);
+    if (!file.exists()) {
+        return false;
+    }
+
+    try (Scanner scanner = new Scanner(file)) {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(", ");
+            if (parts.length == 5) {
+                String carID = parts[0];
+                String brandID = parts[1];
+                String color = parts[2];
+                String frameID = parts[3];
+                String engineID = parts[4];
+                int brandPos = brandList.searchID(brandID);
+                if (brandPos != -1) {
+                    Brand brand = brandList.get(brandPos);
+                    Car car = new Car(carID, brand, color, frameID, engineID);
+                    cars.add(car);
+                }
+            }
+        }
+        return true;
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        return false;
+    }
 }
 
 public boolean saveToFile(String){
-    
+     try {
+        PrintWriter writer = new PrintWriter(filename);
+        for (Car car : cars) {
+            String line = car.getCarID() + ", " + car.getBrand().getBrandID() + ", " +
+                          car.getColor() + ", " + car.getFrameID() + ", " + car.getEngineID();
+            writer.println(line);
+        }
+        writer.close();
+        return true;
+    } catch (IOException e) {
+        e.printStackTrace();
+        return false;
+    }
 }
 
 public int searchID (String ID){
