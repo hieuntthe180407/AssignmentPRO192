ce78090e914cfa46de452757122175ec674e9770
Ducanhhh7777 <147521939+Ducanhhh7777@users.noreply.github.com>
2023-10-23 15:44:26 +0700

    Update BrandList.java

--- SE1866_Group4_AssignmentPRO192/src/BrandList.java
+++ SE1866_Group4_AssignmentPRO192/src/BrandList.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.lang.*;
 public class BrandList{
+     public static void main(String[] args) {
     BrandList brandList = new BrandList();
 
     if (brandList.loadFromFile("brands.txt")) {
